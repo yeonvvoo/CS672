f3f9cae343084f50b55b828adf5c9e9598466df0
Yeonwoo Lim <ywlim@kaist.ac.kr>
2024-12-03 16:07:08 +0900

    clone RL-VLM-F and Gymnasium-Robotics

---
+++ gymnasium_robotics/envs/maze/__init__.py
@@ -0,0 +1,2 @@
+from gymnasium_robotics.envs.maze.ant_maze_v3 import AntMazeEnv
+from gymnasium_robotics.envs.maze.point_maze import PointMazeEnv
---
+++ gymnasium_robotics/envs/multiagent_mujoco/__init__.py
@@ -0,0 +1,8 @@
+"""See the [Doc Page](https://robotics.farama.org/envs/fetch/MaMuJoCo)."""
+
+from gymnasium_robotics.envs.multiagent_mujoco.coupled_half_cheetah import (  # noqa: F401
+    CoupledHalfCheetahEnv,
+)
+from gymnasium_robotics.envs.multiagent_mujoco.mujoco_multi import (  # noqa: F401
+    MultiAgentMujocoEnv,
+)
